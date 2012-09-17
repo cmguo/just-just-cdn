d53900d12deb9e6851fcaeb37cb1452a4f7cc6f2
isxxguo <isxxguo@pptv.com>
2012-09-17 06:17:09 +0000

    /ppbox/branches/1.2.0/src/ppbox mux 采用注册Muxer机制，类名调整
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@4915 b3a1389e-2159-5c43-9038-463bf877d0d9

--- PptvMediaTypes.h
+++ PptvMediaTypes.h
@@ -3,23 +3,9 @@
 #ifndef _PPBOX_CDN_PPTV_MEDIA_TYPES_H_
 #define _PPBOX_CDN_PPTV_MEDIA_TYPES_H_
 
-#include "ppbox/cdn/PptvVod.h"
+#include "ppbox/cdn/PptvVod1.h"
 #include "ppbox/cdn/PptvVod2.h"
 #include "ppbox/cdn/PptvLive2.h"
-
-namespace ppbox
-{
-    namespace cdn
-    {
-
-        PptvMedia * pptv_create_media(
-            boost::asio::io_service & io_svc)
-        {
-            assert(0);
-            return NULL;
-        }
-
-    } // namespace cdn
-} // namespace ppbox
+#include "ppbox/cdn/PptvLive3.h"
 
 #endif // _PPBOX_CDN_PPTV_MEDIA_TYPES_H_
