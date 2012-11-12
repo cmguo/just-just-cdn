eabc3798076813fc884e74b8029cd190b949f84e
isxxguo <isxxguo@pptv.com>
2012-11-12 04:24:45 +0000

    /ppbox/branches/1.2.0 /src/ppbox 使用 ClassFactory
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@5472 b3a1389e-2159-5c43-9038-463bf877d0d9

--- PptvMediaTypes.h
+++ PptvMediaTypes.h
@@ -3,6 +3,8 @@
 #ifndef _PPBOX_CDN_PPTV_MEDIA_TYPES_H_
 #define _PPBOX_CDN_PPTV_MEDIA_TYPES_H_
 
+#define PPBOX_ENABLE_REGISTER_CLASS
+
 #include "ppbox/cdn/PptvVod1.h"
 #include "ppbox/cdn/PptvVod2.h"
 #include "ppbox/cdn/PptvLive2.h"
